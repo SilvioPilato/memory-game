23fa04c26120a0ad4f308fbd77718442b64c8c1b
Silvio Pilato <pilatosil@gmail.com>
2023-05-13 18:17:38 +0200

    Add express server

---
+++ server/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors'
+import path from 'path';
+import { Leaderboard } from '../types';
+import { getRandomLeaderboard } from './utils';
+const port = 8080;
+const app = express();
+const leaderboard: Leaderboard = getRandomLeaderboard();
+
+app.use(express.json());
+app.use(cors()) 
+app.use('/', express.static(path.join(__dirname, '..', 'build')));
+app.get('/', (_, res) => {
+    res.sendFile(path.join('index.html'));
+});
+app.get('/leaderboard', (_, res) => {
+    res.json(leaderboard.sort((a, b) => b.score - a.score));
+});
+app.put('/leaderboard', (req, res) => {
+    const { username, score } = req.body;
+    console.log(req.body)
+    console.log(username, score);
+    leaderboard.push({ username, score });
+    res.json(leaderboard.sort((a, b) => b.score - a.score));
+});
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
---
+++ server/utils/index.ts
@@ -0,0 +1,17 @@
+import { CARD_KINDS, SCORE_DOWN_MODIFIER, SCORE_UP_MODIFIER } from "../../src/config";
+import { Leaderboard } from "../../types";
+import { generateUsername } from "unique-username-generator";
+
+export const getRandomLeaderboard = (users = 10, maxWrongMoves = 7): Leaderboard => {
+    const leaderboard: Leaderboard = [];
+    const maxScore = CARD_KINDS.length * SCORE_UP_MODIFIER;
+    // Add a random score to each user
+    for (let i = 0; i < users; i++) {
+        let deductedPoints = Math.abs(Math.ceil(Math.random() * maxWrongMoves) * SCORE_DOWN_MODIFIER);
+        leaderboard.push({
+            username: generateUsername(),
+            score: maxScore - deductedPoints,
+        });
+    }
+    return leaderboard;
+};
