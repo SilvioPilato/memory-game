9c8da8ab1a2e54539ffcff8756162b276ca20f11
Silvio Pilato <pilatosil@gmail.com>
2023-05-14 17:36:14 +0200

    Changes to config values

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,11 @@
 export const DEFAULT_SHUFFLE_TIMES = 10;
 export const DEFAULT_HIDE_TIME_MS = 500;
+export const DEFAULT_SHOW_TIME_MS = 1000;
+export const DEFAULT_MODAL_SHOW_TIME = 2000;
 export const SCORE_UP_MODIFIER = 10;
 export const SCORE_DOWN_MODIFIER = -5;
 export const CARD_KINDS = [
-    "red" , "blue" , "green" , "yellow" , "black" , "white" , "purple" , "orange"
+    "red" , "blue" , "green" , "yellow" , "black" , "pink" , "brown" , "orange"
 ] as const;
 export const BACKEND_ADDRESS = "http://localhost:8080";
 export const CARDS_PER_KIND = 2;
