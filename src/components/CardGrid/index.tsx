512920c23c79956c742b7016d89ea1e538e2dcd9
Silvio Pilato <pilatosil@gmail.com>
2023-05-14 17:36:54 +0200

    Fixes and changes to style

--- src/components/CardGrid/index.tsx
+++ src/components/CardGrid/index.tsx
@@ -1,6 +1,8 @@
-import React, { useCallback } from 'react';
-import { Card, useAppStore } from '../../store';
-import { SCORE_DOWN_MODIFIER, SCORE_UP_MODIFIER } from '../../config';
+import React, { useCallback, useState } from 'react';
+import { Card, CardKind, useAppStore } from '../../store';
+import { DEFAULT_SHOW_TIME_MS, SCORE_DOWN_MODIFIER, SCORE_UP_MODIFIER } from '../../config';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBugs, faFishFins, faLocust, faSpider, faCat, faDog, faHorse, faShrimp, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 
 export const CardGrid = () => {
@@ -10,10 +12,13 @@ export const CardGrid = () => {
     const setCardShown = useAppStore(state => state.setCardShown);
     const setCardHidden = useAppStore(state => state.setCardHidden);
     const updateScore = useAppStore(state => state.updateScore);
+
+    const [isGridWaiting, setGridWaiting] = useState(false)
     const flipCardMemo = useCallback( (card: Card) => {
         return () =>  {
             // if card is already flipped, do nothing
             if (card.activeSide === 'front') return;
+            if (isGridWaiting) return
             flipCard(card);
             // we have found a match 
             if (cardShown?.kind === card.kind && !cardShown?.hidden ) {
@@ -29,19 +34,50 @@ export const CardGrid = () => {
                 return;
             }
             // the player has flipped two cards that do not match
+            // flip them back over after a delay
+            setGridWaiting(true);
             setCardShown(null);
-            flipCard(card);
-            flipCard(cardShown);
-            updateScore(SCORE_DOWN_MODIFIER)
+            setTimeout(() => {
+                flipCard(card);
+                flipCard(cardShown);
+                updateScore(SCORE_DOWN_MODIFIER)
+                setGridWaiting(false);
+            }, DEFAULT_SHOW_TIME_MS);
         }
-    }, [cardShown, flipCard, setCardHidden, setCardShown, updateScore]);
+    }, [cardShown, flipCard, isGridWaiting, setCardHidden, setCardShown, updateScore]);
     
     const getCardClasses = useCallback((card: Card) => {
-        const baseClasses = 'aspect-square flex place-items-center place-content-center border rounded-md cursor-pointer';
-        const activeSideClasses = card.activeSide === 'front' ? 'bg-blue-500' : 'bg-red-500';
+        const baseClasses = 'aspect-square flex place-items-center rounded-md place-content-center cursor-pointer shadow-xl duration-1000'; 
+        const activeSideClasses = card.activeSide === 'front' ? '[transform:rotateY(180deg)] ' : '';
         const hiddenClasses = card.hidden ? 'invisible' : 'visible'
-        return `${baseClasses} ${activeSideClasses} ${hiddenClasses}`;
+        const color = mapColorsByKind[card.kind];
+        return `${baseClasses} ${activeSideClasses} ${hiddenClasses} ${color}`;
     }, []);
+    const getCardInnerClasses = useCallback((card: Card) => {
+        const baseClasses = 'relative w-full h-full rounded-md [transform-style:preserve-3d] transition-all duration-1000';
+        const activeSideClasses = card.activeSide === 'front' ? '[transform:rotateY(180deg)] ' : '';
+        return `${baseClasses} ${activeSideClasses}`;
+    }, []);
+    const mapIconByKind: Record<CardKind, IconDefinition> = {
+        red: faBugs,
+        blue: faFishFins,
+        green: faLocust,
+        yellow: faDog,
+        black: faSpider,
+        pink: faShrimp,
+        brown: faHorse,
+        orange: faCat
+    };
+    const mapColorsByKind: Record<CardKind, string> = {
+        red: 'text-red-500',
+        blue: 'text-blue-500',
+        green: 'text-green-500',
+        yellow: 'text-yellow-600',
+        black: 'text-black-500',
+        pink: 'text-pink-500',
+        brown: 'text-amber-900',
+        orange: 'text-orange-500'
+    }
 
     return (
         <>
@@ -53,8 +89,32 @@ export const CardGrid = () => {
                                 onClick={flipCardMemo(card)} 
                                 className={getCardClasses(card)}
                                 key={card.id}>
-                                <p>{card.kind}</p>
-                                <p>{card.id}</p>
+                                    <div className={getCardInnerClasses(card)}>
+                                        <div 
+                                            style={{backfaceVisibility:"hidden"}} 
+                                            id="card-front" 
+                                            className='
+                                                bg-slate-50 
+                                                w-full 
+                                                h-full 
+                                                rounded-md 
+                                                [transform:rotateY(180deg)] 
+                                                absolute
+                                                flex
+                                                place-content-center 
+                                                place-items-center
+                                                '
+                                            >
+                                                <FontAwesomeIcon icon={mapIconByKind[card.kind]} size='5x'/>
+                                        </div>
+                                        <div 
+                                            style={{backfaceVisibility:"hidden"}} 
+                                            id="card-back" 
+                                            className='card-back w-full h-full rounded-md absolute'>                                        
+                                        </div>
+
+                                    </div>
+
                             </div>
                         );
                     })
--- src/components/EndGame/index.tsx
+++ src/components/EndGame/index.tsx
@@ -1,7 +1,8 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useAppStore } from '../../store';
-import { Leaderboard } from '../../../types';
-import { BACKEND_ADDRESS } from '../../config';
+import { Leaderboard, LeaderboardEntry } from '../../../types';
+import { BACKEND_ADDRESS, DEFAULT_MODAL_SHOW_TIME } from '../../config';
+import { start } from 'repl';
 
 export const EndGame = () => {
     const [userName, setUserName] = useState('');
@@ -12,6 +13,9 @@ export const EndGame = () => {
     const leaderboard = useAppStore(state => state.leaderboard);
     const setLeaderboard = useAppStore(state => state.setLeaderboard);
     const startNewGame = useAppStore(state => state.startNewGame);
+    const resetDeckSides = useAppStore(state => state.resetDeckSides);
+    const [userInLeaderboard, setUserInLeaderboard] = useState(-1);
+    const [showLeaderboard, setShowLeaderboard] = useState<Leaderboard>([]);
     const changeUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
         setUserName(event.target.value);
     }
@@ -31,10 +35,10 @@ export const EndGame = () => {
         .catch(err => console.log(err));
     }
     const getClasses = useCallback(() => {
-        let showClass = modalShown ? "flex" : "hidden";
-        return `flex-col justify-center items-center ${showClass} z-1 absolute top-0 left-0 w-full h-full bg-gray-500 bg-opacity-50`;
+        const showClass = modalShown ? "flex" : "hidden";
+        return `flex-col justify-center items-center ${showClass} z-1 absolute top-0 left-0 w-full h-full bg-opacity-0`;
     }, [modalShown])
-
+    const scrollToRef = useRef<HTMLInputElement>(null);
     useEffect(() => {
         fetch(`${BACKEND_ADDRESS}/leaderboard`)
         .then(res => res.json()).then((data: Leaderboard) => {
@@ -43,30 +47,61 @@ export const EndGame = () => {
         .catch(err => console.log(err));
     },[setLeaderboard])
 
-
     useEffect(() => {
-        if (cards.every(card => card.hidden)) {
-            showModal()
+        if (cards.every(card => card.hidden) && !modalShown) {
+            const userEntry: LeaderboardEntry = {username: "", score: score};
+            const newLeaderboard: Leaderboard = [...leaderboard, userEntry].sort((a, b) => b.score - a.score);
+            const userIndex = newLeaderboard.findIndex(entry => entry === userEntry);
+            setShowLeaderboard(newLeaderboard);
+            setUserInLeaderboard(userIndex);
+            setTimeout(() => {
+                showModal();
+                resetDeckSides();
+            }, DEFAULT_MODAL_SHOW_TIME);
         }
-    }, [cards, showModal])
+    }, [cards, leaderboard, modalShown, resetDeckSides, score, showModal])
+
+    useEffect(() => {  
+        setTimeout(() => {
+            scrollToRef.current?.scrollIntoView({behavior: 'smooth', block: 'center'})
+            scrollToRef.current?.focus()
+        },2000)
+    }, [userInLeaderboard])
 
     return (
         <div className={getClasses()}>
-            <div>
-                <h1>{"Congratulation, you won!"}</h1>
-                <form onSubmit={onSubmit}>
-                    <input onChange={changeUsername} type="text" placeholder="Enter your name" value={userName}/>
-                </form>
-                {
-                    leaderboard.map((entry, index) => {
-                        return (
-                            <div key={index}>
-                                <span>{entry.username}</span>
-                                <span>{entry.score}</span>
-                            </div>
-                        )
-                    })
-                }
+            <div className='w-96 h-96 px-4 py-2 rounded-xl bg-zinc-50 flex flex-col justify-between'>
+                <h1 className='font-bold text-lg'>{"Congratulation!"}</h1>
+                <h2>{`You won with a score of ${score}`}</h2>
+                <h3>{"Enter your name to save your score"}</h3>
+
+                <div id="leaderboard-content" className='overflow-auto h-56 bg-violet-400 rounded-md'>
+                    {
+                        showLeaderboard.map((entry, index) => {
+                            return (
+                                <div className='h-8 mx-1 my-1 rounded-md bg-violet-500 text-slate-50 px-4 flex justify-between items-center' key={index}>
+                                    <span>{index + 1}</span>
+                                    {
+                                        index === userInLeaderboard &&                 
+                                        <form className='mx-auto font-bold' onSubmit={onSubmit}>
+                                            <input
+                                                autoComplete='off'
+                                                ref={scrollToRef}   
+                                                id='user-input'
+                                                className='bg-transparent border-b-2 border-violet-300 outline-none text-center animate-pulse' 
+                                                onChange={changeUsername} 
+                                                type="text" 
+                                                placeholder="" 
+                                                value={userName}/>
+                                        </form>
+                                    }
+                                    <span>{entry.username}</span>
+                                    <span>{entry.score}</span>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
                 <button onClick={startNewGame}>Start new game</button>
             </div>
         </div>
